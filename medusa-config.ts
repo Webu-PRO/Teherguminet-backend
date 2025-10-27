1252b1bafabf3eab27eded3b0c9d9736181c9b9f
NagyVikt <nagy.viktordp@gmail.com>
2025-10-27 23:32:21 +0100

    new

--- medusa-config.ts
+++ medusa-config.ts
@@ -97,6 +97,22 @@ module.exports = defineConfig({
         ],
       },
     },
+    email: {
+      resolve: "@medusajs/email",
+      options: {
+        provider: "smtp",
+        config: {
+          host: process.env.SMTP_HOST,
+          port: resolveOptionalNumber(process.env.SMTP_PORT) ?? 587,
+          secure: process.env.SMTP_SECURE === "true",
+          auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS,
+          },
+          from: process.env.SMTP_FROM ?? "noreply@therguminet.hu",
+        },
+      },
+    },
     file: {
       resolve: "@medusajs/file",
       options: {
